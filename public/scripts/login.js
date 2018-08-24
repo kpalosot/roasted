c36a93787bc9f2681564c9b80c6050b20b9e003b
Neenus Gabriel <neenus@live.ca>
2018-08-24 15:18:53 +0000

    Login box implemented

---
+++ public/scripts/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#login-btn").click(function () {
+    $(".modal").toggleClass('show');
+    // alert("Login button has been clicked");
+  });
+
+  $(".close").click(function() {
+    $(".modal").removeClass("show");
+  });
+
+  $(".modal").blur (function(){
+    $(".modal").removeClass("show");
+  });
+});
